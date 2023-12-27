5a3175075e26781a4ef77ff90046f0fa8aafa268
Chaplinskii <6945053@gmail.com>
2023-12-27 21:48:13 +0300

    добавили что-то

--- scraping.py
+++ scraping.py
@@ -9,5 +9,12 @@ soup = BeautifulSoup(response.text, "lxml")
 
 data=soup.find("div", class_="w-full rounded border")
 
-name=data.find("h4", class_="")
-print(data)
+name=data.find("h4").text.replace ("/n", "")
+print (name)
+
+price = data.find ("h5").text
+
+print (price)
+
+url_img = "https://scrapingclub.com" + data.find ("img", class_ = "card-img-top img-fluid").get ("src")
+print (url_img)
