270d14c241d44fda10c8f0eb32736f56dc3965fe
Chaplinskii <6945053@gmail.com>
2023-12-28 14:21:27 +0300

    добавили что-то

--- scraping.py
+++ scraping.py
@@ -1,20 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+headers = {User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.1064 YaBrowser/23.9.1.1064 (beta) Yowser/2.5 Safari/537.36}
+for count in range(1, 8):
+    sleep(3)
+    url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
+    
+    response = requests.get(url, headers=headers)
 
-url = "https://scrapingclub.com/exercise/list_basic/?page=1"
+    soup = BeautifulSoup(response.text, "lxml")
 
-response = requests.get(url)
+    data=soup.find_all("div", class_="w-full rounded border")
+    for i in data:
+        name = i.find("h4").text
+        price = i.find ("h5").text
 
-soup = BeautifulSoup(response.text, "lxml")
-
-data=soup.find("div", class_="w-full rounded border")
-
-name=data.find("h4").text.replace ("/n", "")
-print (name)
-
-price = data.find ("h5").text
-
-print (price)
-
-url_img = "https://scrapingclub.com" + data.find ("img", class_ = "card-img-top img-fluid").get ("src")
-print (url_img)
+        url_img = "https://scrapingclub.com" + i.find ("img", class_ = "card-img-top img-fluid").get ("src")
+        print (name + "\n" + price + "\n" + url_img)
---
+++ scraping/scraping.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://scrapingclub.com/exercise/list_basic/?page=1"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "lxml")
+
+data=soup.find("div", class_="w-full rounded border")
+print(data)
